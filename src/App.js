3232d5c4cfe396ddbd1e79df7e8bd4baafc07fb1
Kian Zareinajad <kzareinajad@gmail.com>
2022-12-01 17:32:11 +0000

    footer2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 
-
 import { useEffect, useState } from 'react';
 import { BrowserRouter, Routes , Route, Link  } from 'react-router-dom';
 import Home from './Home'
 import Cart from './Cart'
 import CatInfo from './CatInfo';
 import Navbar from './components/Navbar';
-import Footer from './components/Footer/indexFooter';
+import Footer from './components/Footer/footer';
 import './App.css';
-
 
 function App() {
   const [catData, setCatData] = useState([]);
@@ -17,113 +15,57 @@ function App() {
   //fetch 8 cats
   useEffect(() => {
     const fetchData = async () => {
-      console.log("app:useeffect: fetching data");
+      console.log("app:useeffect: fetching data")
       const response = await fetch(
         "https://api.thecatapi.com/v1/breeds?limit=8"
       );
       const data = await response.json();
-      setCatData(() => {
-        return data;
-      });
-      setLoading(() => {
-        return false;
-      });
+      setCatData(() => { return data });
+      setLoading(() => { return false })
     };
     fetchData();
   }, []);
 
-
-
-  // set the cartData to array of 3 elements(name, price, breed)
-
-
-
-
-  console.log("app: in app");
-
-  console.log("app: in app");
-
   const onAddToCart=(cat,name,price) =>{
     console.log("app: add to cart",name,price,cat)
     let breed=cat.name;
-    let imgSrc = cat.image.url
-    console.log("****",cat)
-    setCartData([...cartData,{name,price,breed,imgSrc}])
+    setCartData([...cartData,{name,price,breed}])
     console.log("app: cartdata, ",cartData)
   }
-  const handleDeleteCat= (cat)=> {
-    let data = [...cartData]
-    console.log("cart:delete cat",cat)
-    const index = data.indexOf(cat);
-    if (index > -1) { // 
-      data.splice(index, 1); // 2nd parameter means remove one item only
-      setCartData(data)
-    }
-}
   console.log("app: in app")
-
-
 
   //if datas empty return to wait for it before allowing routing
   if (!catData || isLoading) {
     console.log("app: no data - waiting", catData.length);
     return;
   }
-  console.log("app: data set length : ", catData.length);
+  console.log("app: data set length : ", catData.length)
   return (
     <>
+    <BrowserRouter>
+      <Navbar id='mainNav'>
 
-      <BrowserRouter>
-        <Navbar id="mainNav">
-          <Link className="navLink" to="/">
-            Home
-          </Link>
-          |<Link to="/CatInfo">Cat Info</Link>|<Link to="/Cart">cart</Link>
-        </Navbar>
+        <Link to = '/'>Home</Link>
+        <Link to = '/CatInfo'>Cat Info</Link>
+        <Link to = '/Cart'>cart</Link>
+      </Navbar>
 
-      
 
-    
-  
-  
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <Home
-                handleAddToCart={onAddToCart}
-                data={catData}
-                cartData={cartData}
-              />
-            }
-          >
+
+        <Routes >
+          <Route path="/" element={<Home handleAddToCart={onAddToCart} data={catData} cartData={cartData}  />}>
             Home
           </Route>
-          <Route
-            path="/CatInfo"
-            element={
-              <CatInfo
-                handleAddToCart={onAddToCart}
-                data={catData}
-                cartData={cartData}
-              />
-            }
-          >
+          <Route path="/CatInfo" element={<CatInfo handleAddToCart={onAddToCart} data={catData}  cartData={cartData} />}>
             cat Info{" "}
           </Route>
-          <Route
-            path="/Cart"
-            element={<Cart cartData={cartData} onDeleteCat={handleDeleteCat} />}
-          >
+          <Route path="/Cart" element={<Cart cartData={cartData} />}>
             cart
           </Route>
         </Routes>
       </BrowserRouter>
 
-      <Footer id="footer">
-        {/* insert footer component here (either as html here or make a react component) */}
-      </Footer>
-
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer/footer.js
@@ -0,0 +1,35 @@
+import React from "react";
+import {
+Box,
+Container,
+Row,
+Column,
+FooterLink,
+Heading,
+} from "./FooterStyles";
+
+const Footer = () => {
+return (
+	<Box>
+	<h1 style={{ color: "green",
+				textAlign: "center",
+				marginTop: "-50px" }}>
+		Cats4LYF: Group A " Dave, Kian , Charine , Max and Jack"
+	</h1>
+	<br/>
+
+	<hr/>
+	<Container>
+	<br/>
+	<br/>
+		<Row>
+		<Heading>About Us</Heading>
+		<Heading>Services</Heading>
+		<Heading>Contact Us</Heading>
+		<Heading>Social Media</Heading>
+		</Row>
+	</Container>
+	</Box>
+);
+};
+export default Footer;
