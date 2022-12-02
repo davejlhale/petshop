631eca410201a909fa1ddb208f269b4f92810ff0
Max Cowen-Carr <maxacowencarr@icloud.com>
2022-12-02 14:58:16 +0000

    updates nav-style

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,27 +1,37 @@
-import React from 'react'
-import { Nav, NavLink, Bars, NavMenu, NavBtn, NavBtnLink } from './NavbarElements';
-import "./icon.css"
+import React from "react";
+import {
+  Nav,
+  NavLink,
+  Bars,
+  NavMenu,
+  NavBtn,
+  NavBtnLink,
+} from "./NavbarElements";
+import "./icon.css";
 
 const Navbar = ({ count, imgIcon }) => {
   return (
     <>
       <Nav>
-        <Bars />
-        <NavLink to='/'>
+        <NavLink to="/">
           <h1>CATS4LYF</h1>
         </NavLink>
         <NavMenu>
-          <NavLink to='/' activeStyle>Home</NavLink>&nbsp&nbsp
+          <NavLink to="/" activeStyle>
+            Home
+          </NavLink>
+          &nbsp&nbsp
           {/* <NavLink to='/Cart' activeStyle>Cart</NavLink>&nbsp&nbsp */}
-          <NavLink to='/CatInfo' activeStyle>CatInfo</NavLink>
+          <NavLink to="/CatInfo" activeStyle>
+            Most In Need
+          </NavLink>
         </NavMenu>
         <NavBtn>
-          <NavBtnLink id="cart-icon" to='/Cart'>
-            <img alt="cat sat in shopping"src={imgIcon} />
+          <NavBtnLink id="cart-icon" to="/Cart">
+            <img alt="cat sat in shopping" src={imgIcon} />
             <p id="cart-item-count">{count}</p>
           </NavBtnLink>
         </NavBtn>
-        <Bars />
       </Nav>
     </>
   );
